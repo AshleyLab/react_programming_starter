9317291070888e202edc296d76b0c0a940a805d2
Jimmy Zhen <jzhen@stanford.edu>
2019-01-31 12:39:36 -0800

    :wrench: Update configs and wrap strings in double quotes

--- webpack.config.test.js
+++ webpack.config.test.js
@@ -1,25 +1,26 @@
-var nodeExternals = require('webpack-node-externals');
+const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
-  target: 'node', // in order to ignore built-in modules like path, fs, etc.
+  mode: "development",
+  target: "node", // in order to ignore built-in modules like path, fs, etc.
   externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: 'babel'
+        use: "babel-loader"
       },
       {
         test: /\.css$/,
         exclude: /node_modules/,
-        loader: 'null-loader'
+        use: "null-loader"
       },
       {
         test: /\.(jpg|png|ttf|eot|woff|woff2|svg)$/,
         exclude: /node_modules/,
-        loader: 'null-loader'
+        use: "null-loader"
       }
     ]
-  },
+  }
 };
