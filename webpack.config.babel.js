2e2abeb515427d2f54557d5bd0428398c943f72b
Jimmy Zhen <jzhen@stanford.edu>
2019-01-31 12:43:57 -0800

    :wrench: Update configs and wrap strings in double quotes

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,75 +1,85 @@
-import path from 'path';
-import webpack from 'webpack';
+const webpack = require("webpack");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-// import ExtractTextPlugin from 'extract-text-webpack-plugin';
-
-let isProduction = process.env.NODE_ENV === 'production';
+let isProduction = process.env.NODE_ENV === "production";
 
 // Development asset host, asset location and build output path.
-const publicHost = isProduction ? '': 'http://localhost:2992';
-const rootAssetPath = './assets';
-const buildOutputPath = './src/build';
+const publicHost = isProduction ? "" : "http://localhost:2992";
+const rootAssetPath = "./assets";
+const buildOutputPath = "./src/build";
 
-let config = {
-  context: path.join(__dirname, 'src'),
-  debug: true,
-  entry: [
-    './index.js'
-  ],
+const config = {
+  mode: isProduction ? "production" : "development",
+  context: path.join(__dirname, "src"),
+  entry: ["./index.js"],
   output: {
-    path: path.resolve(__dirname, 'build/prod/assets'),
-    publicPath: '/assets/',
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "build/prod/assets"),
+    publicPath: "/assets/",
+    filename: "bundle.js"
   },
-  devtool: 'eval-source-map',
+  devtool: "eval-source-map",
   devServer: {
-    contentBase: 'src/public',
+    contentBase: "src/public",
     historyApiFallback: true
   },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
   module: {
-    preLoaders: [
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "eslint-loader"
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'eslint'
-      }
-    ],
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel'
+        use: "babel-loader"
       },
       {
         test: /\.css$/,
         exclude: /node_modules/,
-        loaders: ['style', 'css?modules&sourceMap&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]']
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              context: path.resolve(__dirname, "context"),
+              sourceMap: true,
+              importLoaders: 1,
+              localIdentName: "[name]__[local]___[hash:base64:5]"
+            }
+          }
+        ]
       },
       {
         test: /\.css$/,
         exclude: /src/,
-        loaders: ['style', 'css']
+        use: ["style-loader", "css-loader"]
       },
       {
         test: /\.(jpg|png|ttf|eot|woff|woff2|svg)$/,
         exclude: /node_modules/,
-        loader: 'url?limit=100000'
+        use: "url-loader?limit=100000"
       }
     ]
   }
 };
 
 if (isProduction) {
-  config.devtool = 'source-map';
+  config.devtool = "source-map";
   config.devServer = {};
   config.plugins = [
     new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
+      "process.env": {
+        "NODE_ENV": JSON.stringify("production")
       }
     }),
-    new ExtractTextPlugin('[name].[chunkhash].css')
-  ]
+    new MiniCssExtractPlugin("[name].[chunkhash].css")
+  ];
 }
 
-export default config;
+module.exports = config;
