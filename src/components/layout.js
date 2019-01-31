2ed0a939f5c191881e7425597d63779eb49d0686
Jimmy Zhen <jzhen@stanford.edu>
2019-01-31 12:24:18 -0800

    :wrench: Wrap strings with double quotes; set up routes and removed unused propsTypes

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,46 +1,58 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router';
+import React, { Component } from "react";
+import { Switch, Route, Link } from "react-router-dom";
 
-import style from './style.css';
+import Home from "./home";
+import PapersIndex from "./papers";
+import PapersShow from "./papers/show";
+// import Example from "./example";
+
+import style from "./style.css";
 
 class Layout extends Component {
-  render () {
+  render() {
     return (
       <div>
-        <nav className='navbar navbar-inverse navbar-fixed-top'>
-          <div className='container-fluid'>
-            <div className='navbar-header'>
-              <button aria-controls='navbar' aria-expanded='false' className='navbar-toggle collapsed' data-target='#navbar' data-toggle='collapse' type='button'>
-                <span className='sr-only'>Toggle navigation</span>
-                <span className='icon-bar' />
-                <span className='icon-bar' />
-                <span className='icon-bar' />
+        <nav className="navbar navbar-inverse navbar-fixed-top">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <button aria-controls="navbar" aria-expanded="false" className="navbar-toggle collapsed" data-target="#navbar" data-toggle="collapse" type="button">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar" />
+                <span className="icon-bar" />
+                <span className="icon-bar" />
               </button>
-              <a className='navbar-brand' href='#'>Home</a>
+              <a className="navbar-brand" href="#">
+                Home
+              </a>
             </div>
-            <div className='navbar-collapse collapse' id='navbar'>
-              <form className='navbar-form navbar-right'>
-                <input className='form-control' placeholder='Search...' type='text' />
+            <div className="navbar-collapse collapse" id="navbar">
+              <form className="navbar-form navbar-right">
+                <input className="form-control" placeholder="Search..." type="text" />
               </form>
             </div>
           </div>
         </nav>
-        <div className='container-fluid'>
+        <div className="container-fluid">
           <div className={`row application-row ${style.container}`}>
-            <div className='col-sm-12 main'>
-              <div className='row'>
-                <div className='col-sm-2 main'>
-                  <ul className='nav nav-pills nav-stacked'>
+            <div className="col-sm-12 main">
+              <div className="row">
+                <div className="col-sm-2 main">
+                  <ul className="nav nav-pills nav-stacked">
                     <li>
-                      <Link to='/'>Home</Link>
+                      <Link to="/">Home</Link>
                     </li>
                     <li>
-                      <Link to='papers'>Papers</Link>
+                      <Link to="/papers">Papers</Link>
                     </li>
                   </ul>
                 </div>
-                <div className='col-sm-10 main'>
-                  {this.props.children}
+                <div className="col-sm-10 main">
+                  <Switch>
+                    <Route component={Home} exact path="/" />
+                    <Route component={PapersIndex} path="/papers" />
+                    <Route component={PapersShow} path="/papers/:id" />
+                    {/* <Route component={Example} path="/example" /> */}
+                  </Switch>
                 </div>
               </div>
             </div>
@@ -50,9 +62,5 @@ class Layout extends Component {
     );
   }
 }
-
-Layout.propTypes = {
-  children: React.PropTypes.object
-};
 
 export default Layout;
