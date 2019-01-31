efdadb1ced51c66a1da2d7c386bfcbec517a40ab
Jimmy Zhen <jzhen@stanford.edu>
2019-01-31 12:31:11 -0800

    :wrench: Update assertions due to dependency changes and wrap strings in double quotes

--- src/components/papers/papers.test.js
+++ src/components/papers/papers.test.js
@@ -1,20 +1,20 @@
-import assert from 'assert';
-import React from 'react';
-import TestUtils from 'react-addons-test-utils';
+import assert from "assert";
+import React from "react";
+import ShallowRenderer from "react-test-renderer/shallow";
 
-import PapersIndex from './index';
+import PapersIndex from "./index";
 
-describe('PapersIndex', () => {
-  const renderer = TestUtils.createRenderer();
+describe("PapersIndex", () => {
+  const renderer = new ShallowRenderer();
   renderer.render(<PapersIndex />);
-  const output = renderer.getRenderOutput();
-  const children = output.props.children;
+  const result = renderer.getRenderOutput();
+  const children = result.props.children;
 
-  it('should be able to render two immediate child nodes', () => {
+  it("should be able to render two immediate child nodes", () => {
     assert.equal(children.length, 2);
   });
 
-  it('should be able to render one <h1> node', () => {
-    assert.equal(children[0].type, 'h1');
+  it("should be able to render one <h1> node", () => {
+    assert.equal(children[0].type, "h1");
   });
 });
