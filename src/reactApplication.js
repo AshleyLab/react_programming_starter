29390eeab97d9d5aff0342e51da1da7a342c2e79
Jimmy Zhen <jzhen@stanford.edu>
2019-01-31 16:33:59 -0800

    :wrench: Fixed unit test failure

--- src/reactApplication.js
+++ src/reactApplication.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import { Router, Route } from "react-router-dom";
-import _history from "./history";
+import { createMemoryHistory } from "history";
+import browserHistory from "./history";
 import Layout from "./components/layout";
 
 class ReactApp extends Component {
   render() {
+    let _history = (typeof history === "object") ? browserHistory : createMemoryHistory("/");
     return (
       <Router history={_history}>
         <Route component={Layout} path="/" />
